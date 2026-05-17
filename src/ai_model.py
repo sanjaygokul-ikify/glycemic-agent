43c28433bba8a2bebd0c7b38350c142425f91977
Sanjay G <gokulesh2615@gmail.com>
2026-05-17 18:51:38 +0530

    feat: add src/ai_model.py

---
+++ src/ai_model.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class AIModel:
+    def __init__(self, **kwargs):
+        pass
+    def fit(self, X, y):
+        # Train the model
+        pass
+    def predict(self, X):
+        # Make predictions
+        pass
